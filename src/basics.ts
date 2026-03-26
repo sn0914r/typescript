42333477b68eb01f5548a0aed94493835cb3b2bd
sn0914r <reddysivananda83@gmail.com>
2026-03-26 22:17:24 +0530

    learn: tuples and nested tuples

--- src/basics.ts
+++ src/basics.ts
@@ -26,7 +26,33 @@ value = 5;
 
 // ==== function types ====
 function greet(name: string): string {
-  return `Hello ${name}`
+  return `Hello ${name}`;
 }
 
 console.log(greet("sivananda"));
+
+// ==== tuples (it is a structured array) ====
+const interviewDetails: [string, string, number] = ["Google", "SDE-1", 45];
+
+let [companyName, role, duration] = interviewDetails;
+console.log(
+  "\ncompany name: ",
+  companyName,
+  "\nrole: ",
+  role,
+  "\nduration: ",
+  duration,
+  "\n",
+);
+
+// ==== nested tuples ====
+const studentDetails: [string, number][] = [
+  ["Sivananda", 15],
+  ["Karam Venu", 20],
+  ["gopal", 24],
+];
+
+studentDetails.forEach((student) => {
+  let [studentName, rollNumber] = student;
+  console.log(`${studentName} - ${rollNumber}`);
+});
